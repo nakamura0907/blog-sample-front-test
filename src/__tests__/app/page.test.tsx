0ad64912ca7540285c06bea2227030ea99d101ad
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-05-18 00:28:58 +0900

    jest環境の構築

---
+++ src/__tests__/app/page.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react';
+import Home from '@/app/page';
+
+describe('Home', () => {
+  it('renders Deploy now button', () => {
+    render(<Home />);
+
+    const button = screen.getByRole('link', {
+      name: /deploy now/i,
+    });
+
+    expect(button).toBeInTheDocument();
+  });
+});
