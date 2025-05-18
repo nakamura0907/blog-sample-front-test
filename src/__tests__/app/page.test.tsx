265fecab7d662b34c40bfa63fb5a2df6a3b342c7
nakamura0907 <53244048+nakamura0907@users.noreply.github.com>
2025-05-18 10:30:09 +0900

    単体テストジョブの追加と複合アクションの作成

--- src/__tests__/app/page.test.tsx
+++ src/__tests__/app/page.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
 
-describe('Home page', () => {
+describe('Home', () => {
   it('renders Deploy now button', () => {
     render(<Home />);
 
